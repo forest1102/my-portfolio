0a3a1aaa40c980dc385f4ca72f72f72f0728410f
issei morita <isseimorita1@gmail.com>
2019-07-03 22:58:05 -0700

    enable to use vue router

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
-const express = require('express');
-const port = process.env.PORT || 8080;
-const app = express();
-app.use(express.static(__dirname + "/dist/"));
-app.listen(port);
+const express = require('express')
+const history = require('connect-history-api-fallback')
+const path = require('path')
+const serveStatic = require('serve-static')
+
+
+const app = express()
+
+app.use(history({
+	verbose: true
+}))
+
+app.use(express.static(path.join(__dirname, '/dist')))
+
+var port = process.env.PORT || 5000
+
+app.listen(port, () => {
+	console.log('Server started at http://localhost:5000')
+})
