695c9f8dd15d0f395b11ff5fd09b864a331d43e2
issei morita <isseimorita1@gmail.com>
2019-07-13 20:18:22 -0700

    change corresponding text color when scrolling

--- src/components/Drawer.vue
+++ src/components/Drawer.vue
@@ -7,78 +7,48 @@ v-navigation-drawer(
     :mobile-break-point="$vuetify.thresholds.sm"
     v-bind:value="$vuetify.breakpoint.mdAndUp?true:value" 
     v-on:input="$emit('input', $event)" 
+    v-scroll="scroll"
   )
   v-toolbar(
     flat
     prominent
     ).transparent
-    v-list(two-line)
-      v-list-tile-content
-        v-list-tile-title.bold.teal--text.headline.text-sm-center {{$t('name')}}
+    v-list
+      v-list-tile(@click="toTop")
+        v-list-tile-content
+          v-list-tile-title.bold.teal--text.headline.text-sm-center {{$t('name')}}
   v-list
     v-divider
     v-list-tile(
-      v-for="menu in $t('menu')"
-      :key="menu.title"
+      v-for="menu in menus"
+      :key="menu.to"
       @click="link(menu.to)"
       )
       v-list-tile-action
-        v-icon(:color="(menu.to===$route.hash)? 'teal':''") {{menu.icon}}
+        v-icon(:color="(menu.to===curViewElement)? 'teal':''") {{menu.icon}}
       v-list-tile-content
-        v-list-tile-title.subheading(:class="(menu.to===$route.hash)? 'teal--text':''") {{menu.title}}
+        v-list-tile-title.subheading(:class="(menu.to===curViewElement)? 'teal--text':''") {{$t(`menu['${menu.to}']`)}}
 </template>
 
 <i18n>
   {
     "en":{
       "name":"Issei Morita",
-      "menu":[
-        {
-          "title": "About",
-          "to": "#about",
-          "icon": "person"
-        },
-        {
-          "title": "Skills",
-          "to": "#skill",
-          "icon": "format_list_bulleted"
-        },
-        {
-          "title": "Timeline",
-          "to": "#timeline",
-          "icon": "show_chart"
-        },
-        {
-          "title": "Contact",
-          "to": "#contact",
-          "icon": "share"
-        }
-      ]
+      "menu":{
+          "#about": "About",
+          "#skill": "Skills",
+          "#timeline": "Timeline",
+          "#contact": "Contact"
+      }
     },
     "ja":{
       "name":"森田一世",
-      "menu":[
-        {
-          "title": "自己紹介",
-          "to": "#about",
-          "icon": "person"
-        },
-        {
-          "title": "開発可能言語",
-          "to": "#skill",
-          "icon": "format_list_bulleted"
-        },
-        {
-          "title": "経歴",
-          "to": "#timeline",
-          "icon": "show_chart"
-        },
-        {
-          "title": "コンタクト",
-          "to": "#contact",
-          "icon": "share"
-        }
-      ]
+      "menu":{
+          "#about": "自己紹介",
+          "#skill": "開発可能言語",
+          "#timeline": "経歴",
+          "#contact": "コンタクト"
+      }
     }
   }
 </i18n>
@@ -90,7 +60,59 @@ import languages from '@/data/languages.json'
 @Component({})
 export default class extends Vue {
   @Prop() public value!: boolean
+
+  private readonly menus = [
+    {
+      to: '#about',
+      icon: 'person'
+    },
+    {
+      to: '#skill',
+      icon: 'format_list_bulleted'
+    },
+    {
+      to: '#timeline',
+      icon: 'show_chart'
+    },
+    {
+      to: '#contact',
+      icon: 'share'
+    }
+  ]
   private readonly languages = languages
+
+  private isMounted = false
+
+  private scrollCenter: number = 0
+
+  protected mounted() {
+    this.isMounted = true
+  }
+
+  private get selectors(): { selector: HTMLElement, to: string } [] {
+    return (this.isMounted) ?
+      this.menus
+      .map(({ to }) => ({
+        selector: (document.querySelector(to) as HTMLElement),
+        to
+      }))
+      .filter(({ selector }) => selector) : []
+  }
+
+  private get curViewElement() {
+    return (this.selectors
+      .find(({ selector, to }) => {
+        const top = selector.offsetTop
+        const bottom = top + selector.offsetHeight
+        return top < this.scrollCenter && this.scrollCenter < bottom
+      }) || { to: '' }).to
+  }
+
+  private toTop() {
+    this.$vuetify.goTo('#home')
+    this.$router.replace({ hash: '' })
+  }
+
   private link(to: string) {
     this.$vuetify.goTo(to)
     this.$router.replace({ hash: to })
@@ -99,8 +121,16 @@ export default class extends Vue {
     return this.languages.find(e => e.locale === this.$i18n.locale) || { name: '', title: '' }
   }
 
+  private offset(id: string) {
+    return document.querySelector(id)
+  }
+
   private changeLocale(locale: string) {
     this.$router.push({ params: { locale } })
+  }
+
+  private scroll() {
+    this.scrollCenter = (window.pageYOffset || document.documentElement.scrollTop) + this.$vuetify.breakpoint.height / 2
   }
 }
 </script>
